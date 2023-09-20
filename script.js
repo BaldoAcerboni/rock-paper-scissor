f802497cfed470451da3e0932f90a5ac9cd66263
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-20 16:29:54 +0200

    Modify UI from prompt to buttons

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
-const playRoundButton = document.querySelector('#round-button');
 const possibleChoice = ['rock', 'paper', 'scissor'];
-const allowedChoice = /^rock$|^paper$|^scissor$/i;
-let roundResultMessage = '';
+const buttons = document.getElementsByClassName('btn');
+const retry = document.getElementById('retry');
+let roundResultMessage = document.getElementById('round-result-message');
+const partialScoreMessage = document.getElementById('partial-score');
 let totalScoreCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+
+for(let button of buttons) {
+    button.addEventListener('click', playRound)
+}
 
 function getComputerChoice() {
     return possibleChoice[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerChoice, computerChoice) {
-   
-    if(allowedChoice.test(playerChoice)) {
-        if (computerChoice === playerChoice) {
-            roundResultMessage = 'Draw!';
-        } else if (computerChoice === 'rock') {
-            if (playerChoice === 'paper') {
-                roundResultMessage = 'You win, paper beats rock!';
-                totalScoreCount++;
-            } else { // player = scissor
-                roundResultMessage = 'You lose, rock beats scissor!';
-                totalScoreCount--;
-            }
-        } else if (computerChoice === 'paper') {
-            if (playerChoice === 'rock') {
-                roundResultMessage = 'You lose, paper beats rock!';
-                totalScoreCount--;
-            } else { //player = scissor
-                roundResultMessage = 'You win, scissor beats paper!';
-                totalScoreCount++;
-            }
-        } else { // computer = scissor
-            if (playerChoice === 'rock') {
-                roundResultMessage  = 'You win, rock beats scissor!';
-                totalScoreCount++;
-            } else {  // player = paper
-                roundResultMessage = 'You lose, scissor beats paper!';
-                totalScoreCount--;
-            }
+function playRound(e) {
+    const playerChoice = e.target.textContent;
+    const computerChoice = getComputerChoice();
+    
+    if (computerChoice === playerChoice) {
+        roundResultMessage.textContent = 'Draw!';
+    } else if (computerChoice === 'rock') {
+        if (playerChoice === 'paper') {
+            roundResultMessage.textContent = 'You win, paper beats rock!';
+            playerScore++;
+        } else { // player = scissor
+            roundResultMessage.textContent = 'You lose, rock beats scissor!';
+            computerScore++;
         }
-    } else {
-        roundResultMessage = `You can't even type?!?(that count as a loss BTW)`;
-        totalScoreCount--;
+    } else if (computerChoice === 'paper') {
+        if (playerChoice === 'rock') {
+            roundResultMessage.textContent = 'You lose, paper beats rock!';
+            computerScore++;
+        } else { //player = scissor
+            roundResultMessage.textContent = 'You win, scissor beats paper!';
+            playerScore++;
+        }
+    } else { // computer = scissor
+        if (playerChoice === 'rock') {
+            roundResultMessage.textContent  = 'You win, rock beats scissor!';
+            playerScore++;
+        } else {  // player = paper
+            roundResultMessage.textContent = 'You lose, scissor beats paper!';
+            computerScore++;
+        }
     }
+    playGame()
+    
 }
 
 function playGame() {
-    for(let i = 0; i < 5; i++) {
-        const playerChoice = 
-            prompt('Please choose between rock paper and scissor', '').toLowerCase();
-        const computerChoice = getComputerChoice();
-        playRound(playerChoice, computerChoice)
-        console.log(roundResultMessage);
-    }
-    if (totalScoreCount > 0) {
-        console.log('congratulations, you beat the machine')
-    } else if (totalScoreCount < 0) {
-        console.log('the machine won, you loser')
+    partialScoreMessage.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+    if(playerScore < 3 && computerScore < 3) { 
+        return; 
     } else {
-        console.log('that is a perfect draw')
+        if(playerScore === 3) {
+        partialScoreMessage.textContent = 'Congratulations you won!!!try again?'
+        } else if(computerScore === 3) {
+            partialScoreMessage.textContent = 'You lost! Try again?'
+        }
+        for(let button of buttons) {
+            button.removeEventListener('click', playRound)
+        }
     }
 }
-
-playRoundButton.addEventListener('click', playGame);
