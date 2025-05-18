f32170e3bb3121e6fb889842ff67fe17266216be
Gnagno <baldo.acerboni@gmail.com>
2025-05-18 15:23:49 +0200

    HTML + JS done

---
+++ script.js
@@ -0,0 +1,84 @@
+const hand = ["rock", "paper", "scissor"];
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
+const result = document.querySelector(".result");
+const handsPlayed = document.querySelector(".hands");
+const scorePara = document.querySelector(".score");
+let userHand;
+let score = [0, 0];
+
+function randomChoice() {
+  return hand[Math.floor(Math.random() * 3)];
+}
+
+function checkWin(user, computer) {
+  updateScore();
+  if (user === computer) {
+    result.textContent = "It is a draw";
+    console.log("draw");
+  } else if (user === "rock") {
+    if (computer === "paper") {
+      result.textContent = "Computer wins the round";
+      updateScore(1);
+      console.log("computer");
+    } else if (computer === "scissor") {
+      console.log("user wins");
+      result.textContent = "User wins the round";
+      updateScore(0);
+    }
+  } else if (user === "paper") {
+    if (computer === "rock") {
+      result.textContent = "User wins the round";
+      updateScore(0);
+      console.log("user wins");
+    } else if (computer === "scissor") {
+      result.textContent = "Computer wins the round";
+      updateScore(1);
+      console.log("the way of the machine");
+    }
+  } else {
+    if (computer === "rock") {
+      result.textContent = "Computer wins the round";
+      updateScore(1);
+      console.log("the way of the machine");
+    } else if (computer === "paper") {
+      result.textContent = "User wins the round";
+      updateScore(0);
+      console.log("user wins");
+    }
+  }
+  handsPlayed.textContent = `User: ${user}, Computer: ${computer}`;
+}
+
+function updateScore(n) {
+  if (n !== undefined) {
+    score[n]++;
+  }
+  scorePara.textContent = `User`;
+  if (score[n] === 2) {
+    if (n === 0) {
+      scorePara.textContent = "User wins the game";
+    } else {
+      scorePara.textContent = "Computer wins the game";
+    }
+    score = [0, 0];
+  } else {
+    scorePara.textContent = `SCORE: User: ${score[0]} Computer: ${score[1]}`;
+  }
+}
+
+rockBtn.addEventListener("click", () => {
+  userHand = "rock";
+  checkWin(randomChoice(), userHand);
+});
+
+paperBtn.addEventListener("click", () => {
+  userHand = "paper";
+  checkWin(randomChoice(), userHand);
+});
+
+scissorBtn.addEventListener("click", () => {
+  userHand = "scissor";
+  checkWin(randomChoice(), userHand);
+});
